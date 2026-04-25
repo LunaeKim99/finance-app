d8300c1639cae21732ace6d8dba60ec33d0b9b6d
YourName <your.email@example.com>
2026-04-25 22:02:57 +0700

    chore: exclude app_config.dart from repo

--- lib/config/app_config.dart
+++
@@ -1,9 +0,0 @@
-class AppConfig {
-  AppConfig._();
-
-  static const bool isDemoBuild = true;
-
-  static String get buildLabel => isDemoBuild ? 'DEMO' : '';
-
-  static bool get allFeaturesUnlocked => isDemoBuild;
-}
